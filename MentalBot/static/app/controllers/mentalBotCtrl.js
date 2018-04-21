e2fc44baefc9bf7b23b36b8db7818815c15aa12c
TheDriftingWind <CharlesYZhu@yahoo.com>
2018-04-21 16:18:33 -0400

    final

--- MentalBot/static/app/controllers/mentalBotCtrl.js
+++ MentalBot/static/app/controllers/mentalBotCtrl.js
@@ -1,7 +1,12 @@
 app.controller("mentalBotCtrl", ["mentalBotSvc","$scope", "$rootScope", function(mentalBotSvc, $scope, $rootScope){
-$scope.test = test;
-$scope.emo = ''
-$scope.countDown = 2;
+$scope.emo = '';
+$scope.countDown = 10;
+$scope.convo = 0;
+$scope.setConvo = setConvo
+$scope.showdebug = '1';
+$scope.toggleDebug = toggleDebug;
+$scope.allowShowChange = true;
+$scope.finishConvo = finishConvo;
 
 //$scope.e = $rootScope.emotion
 console.log($rootScope.emotion)
@@ -12,15 +17,47 @@ var timer = setInterval(function(){
   } else {
     $scope.emo = $rootScope.emotion
     //test()
-    $scope.countDown = 2;
+    $scope.countDown = 10;
   }
-  $scope.$apply(function() {
-      $scope.show = $scope.emo
-  });
-},1000);
+  if($scope.allowShowChange == true){
+    $scope.$apply(function() {
+        $scope.show = $scope.emo
+        $scope.allowShowChange = false;
+        switch($scope.emo){
+          case 0: setConvo(30)
+            break;
+          case 1: setConvo(10)
+            break;
+          case 2: setConvo(20);
+            break;
+        }
+    });
+  }
+},10000);
 
-function test(){
-  console.log($rootScope.emotion)
+// function test(){
+//   console.log($rootScope.emotion)
+// }
+
+function setConvo(val){
+  $scope.convo = val;
 }
+
+function setShow(val) {
+  $scope.show = val;
+}
+
+function toggleDebug(){
+  if($scope.showdebug == 0){
+        $scope.showdebug = 1;
+  } else {
+      $scope.showdebug = 0;
+  }
+}
+
+function finishConvo(){
+  $scope.allowShowChange = true;
+}
+
 
 }]);
